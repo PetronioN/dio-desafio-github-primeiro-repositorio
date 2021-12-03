c56076688260eb9edc4edb64470c454756a9637c
Petronio <petroniorocha81@gmail.com>
2021-12-02 21:32:59 -0300

    Adicao de novos arquivos

---
+++ MRV-fullstack-developer/Introdução ao JavaScript/Recursos básicos da linguagem/funcoes.js
@@ -0,0 +1,15 @@
+function soma(a, b){
+    console.log(a + b);
+    return a + b; //8
+}
+
+soma(3, 5);
+
+/*O parênteses é o local para você declarar
+parâmetros da sua função.
+
+O uso do return é normalmente para quando você
+precisa usar o resultado de uma função em outra.
+
+O console.log é quando você quer ver se aquilo está
+funcionando. É muito utilizado para debugar.*/
---
+++ MRV-fullstack-developer/Introdução ao JavaScript/Recursos básicos da linguagem/manipulando um arquivo.js
@@ -0,0 +1,23 @@
+//Isso é um comentário de uma linha
+/*Isso é um
+código de várias linhas*/
+
+var y = 0; //a criação de uma variável inicia com var
+
+/*É uma boa prática você dar nomes que tenham sentido
+a suas variáveis*/
+
+var preco = 2;
+var desconto = 0.2;
+/*const PRECO = 2; uma constante é declarada com
+letras maiúsculas, isso é uma boa prática.
+
+O valor de uma constante não pode ser alterado.
+Você não pode fazer PRECO = PRECO - desconto;
+
+Mas eu tenho a possibilidade de fazer o seguinte:
+var desconto = 0.2;
+const PRECO = 2;
+var total = PRECO - desconto;*/
+
+preco = preco - desconto
