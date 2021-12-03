6613413d5ddd97bda82e0852eb6d81318e338636
Petronio <petroniorocha81@gmail.com>
2021-12-02 21:10:49 -0300

    Atualizacao da pasta Introducao ao Javascript

---
+++ MRV-fullstack-developer/Introdução ao JavaScript/Colocando em prática/Desafio Contador/assets/js/scripts.js
@@ -0,0 +1,24 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment(){
+    if (currentNumber < 10) {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+
+    else if (currentNumber >= 10) {
+        currentNumber = 10;
+    }
+}
+
+function decrement(){
+    if (currentNumber > 0) {
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+
+    else if (currentNumber <= 0) {
+        currentNumber = 0;
+    }
+}
---
+++ MRV-fullstack-developer/Introdução ao JavaScript/Console/Executando um arquivo.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+/*Para vermos a saída desse comando, abrimos o terminal e possivelmente ele será aberto
+no powershell, minha preferência é bash, para mudar identifique uma setinha para baixo
+que fica ao lado do simbolo + no canto inferior direito e selecione bash.
+
+Com o bash aberto, digite node (nome do seu arquivo) e ele irá rodar. Ex.:
+
+node Executando\ um\ arquivo.js 
+
+As barras no meu exemplo são para o uso de espaços que eu coloque no nome do arquivo .js*/
